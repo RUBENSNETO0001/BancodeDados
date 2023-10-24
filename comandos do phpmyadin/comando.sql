40a30893c1817688e249c2c1f4c58026864b7886
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-10-24 14:58:36 -0500

    efsdfsf

---
+++ comandos do phpmyadin/comando.sql
@@ -0,0 +1,19 @@
+/*primary key*/
+CREATE TABLE endereco(
+ endereco_Id INT AUTO_INCREMENT PRIMARY KEY, 
+ numero_rua INT,
+ nome_rua varchar(222),
+ cidade varchar(111)
+);
+
+
+/*recebedor*/
+CREATE TABLE usuario(
+ Id INT AUTO_INCREMENT PRIMARY KEY, 
+  name VARCHAR(122) NOT NULL,   
+     endereco_ID INT,
+    FOREIGN KEY(endereco_ID) REFERENCES endereco(endereco_ID)
+);
+
+
+

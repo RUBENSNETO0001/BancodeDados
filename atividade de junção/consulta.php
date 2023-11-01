f1f63063c5414c8c67aeeebe8e9f62fd47e7b3c4
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-11-01 15:09:58 -0500

    EWD

--- atividade de junção/consulta.php
+++ atividade de junção/consulta.php
@@ -11,13 +11,13 @@ $resultados = $consulta_sql->fetch_all(MYSQLI_ASSOC);
 ECHO '<PRE>';
 print_r( $resultados);
 ECHO'</PRE>';
-
+/////////////////////////////////////////////
 foreach($resultados as $pessoas){
     echo $pessoas['nome']. "<br>";
     echo $pessoas['email']. "<br>";
     echo "<br>";
 }
-
+///////////////////////////////////
 $consulta_sql = $conexao->query("SELECT * FROM usuario");
 
 $um_usuario = $consulta_sql->fetch_assoc();
@@ -26,5 +26,21 @@ $um_usuario = $consulta_sql->fetch_assoc();
 ECHO '<PRE>';
 print_r( $um_usuario);
 ECHO'</PRE>';
+
+ /////////////////////////
+$nome = "gabigol";
+$email = "gabigol@gmail.com";
+$senha = "125";
+
+$inserir_dados= $conexao->query("INSERT INTO usuario(nome , senha , email) 
+VALUES ('$nome','$senha', '$email')");
+
+if($inserir_dados == true){
+    echo'dados inseridos com sucesso';
+}
+
+else {
+    echo'nao ta inserido';
+}
 
 ?>

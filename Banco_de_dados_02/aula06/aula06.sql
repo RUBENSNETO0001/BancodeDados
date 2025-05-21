088a5424dd03cc7c3baa65a90d25eae2d2169b18
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2025-05-21 17:22:04 -0300

    in e sub select

---
+++ Banco_de_dados_02/aula06/aula06.sql
@@ -0,0 +1,48 @@
+SELECT aluno_id 
+FROM inscricoes 
+WHERE nota > (SELECT AVG(nota) FROM inscricoes WHERE curso_id = inscricoes.curso_id);
+-- aqui to vendo se os alunos com nota maior do curso serem mostrada
+
+--Notas acima da média do curso
+
+SELECT nome
+FROM Alunos
+WHERE aluno_id IN (
+    SELECT aluno_id
+    FROM Inscricoes
+    WHERE nota > (
+        SELECT AVG(nota)
+        FROM Inscricoes
+        WHERE curso_id = Inscricoes.curso_id
+    )
+);
+
+--Cursos com mais de uma inscrição
+SELECT nome_curso
+FROM Cursos
+WHERE curso_id IN (
+    SELECT curso_id
+    FROM Inscricoes
+    GROUP BY curso_id
+    HAVING COUNT(aluno_id) > 1
+);
+
+--Nota Média de Cada Aluno:
+SELECT nome, (
+    SELECT AVG(nota)
+    FROM Inscricoes
+    WHERE Inscricoes.aluno_id = Alunos.aluno_id
+) AS media_notas
+FROM Alunos;
+
+--Alunos com Notas Acima de 80 em Todos os Cursos:
+
+SELECT nome
+FROM Alunos
+WHERE aluno_id NOT IN (
+    SELECT aluno_id
+    FROM Inscricoes
+    WHERE nota <= 80
+);
+
+SELECT nome  FROM alunos WHERE aluno_id IN (SELECT aluno_id from inscricoes WHERE nota > 80);
